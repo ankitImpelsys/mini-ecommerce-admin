bd6682f3497821fa89b367450d2be7a70212fac8
ankitImpelsys <ankit.a@impelsys.com>
2025-07-12 01:25:41 +0530

    .

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,10 +6,12 @@ use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
-class ProductType extends AbstractType
+class   ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -23,7 +25,22 @@ class ProductType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'id',
             ])
-        ;
+            ->add('imageFile', FileType::class, [
+                'label' => 'Product Image (optional)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image (JPEG, PNG, GIF)',
+                    ])
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
