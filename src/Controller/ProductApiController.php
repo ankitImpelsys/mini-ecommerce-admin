9a518b5b3bb642fe97cf264c76123191ce14ca49
ankitImpelsys <ankit.a@impelsys.com>
2025-07-16 14:33:04 +0530

    UI Improvement

---
+++ src/Controller/ProductApiController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/api/products')]
+final class ProductApiController extends AbstractController
+{
+    #[Route('', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): JsonResponse
+    {
+        $products = $productRepository->findAll();
+
+        $data = [];
+        foreach ($products as $product) {
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+                'stock' => $product->getStock(),
+                'image' => $product->getImageFilename(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(Product $product): JsonResponse
+    {
+        $data = [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'price' => $product->getPrice(),
+            'stock' => $product->getStock(),
+            'image' => $product->getImageFilename(),
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setDescription($data['description'] ?? null);
+        $product->setPrice($data['price']);
+        $product->setStock($data['stock']);
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Product created!', 'id' => $product->getId()]);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function update(Request $request, Product $product, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product->setName($data['name']);
+        $product->setDescription($data['description'] ?? null);
+        $product->setPrice($data['price']);
+        $product->setStock($data['stock']);
+
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Product updated!']);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Product $product, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Product deleted']);
+    }
+}
