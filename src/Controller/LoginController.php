0beb4a37699895e6f4b9808d8c45689d410ec4dc
ankitImpelsys <ankit.a@impelsys.com>
2025-08-03 13:50:07 +0530

    cache clear after logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,9 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-#[Route('/login', name: 'login')]
 final class LoginController extends AbstractController
 {
+    #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // If user is already logged in, redirect to admin
@@ -23,14 +23,23 @@ final class LoginController extends AbstractController
         // Last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/login.html.twig', [
+        $response = $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+
+        // Set headers to prevent caching of login page
+        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate, private, max-age=0');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+
+        return $response;
     }
 
+    #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This will be intercepted by Symfony logout.');
+        // This method can be blank - it will be intercepted by the logout key on your firewall
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
